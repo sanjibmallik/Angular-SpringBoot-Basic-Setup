290fff53bedaea476b28a6da8f2e6bea2b752c74
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2020-03-11 23:55:44 +0530

    save

--- src/main/java/com/example/demo/controller/DemoWhileLabelErrorController.java
+++ src/main/java/com/example/demo/controller/DemoWhileLabelErrorController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MyErrorController implements ErrorController  {
+public class DemoWhileLabelErrorController implements ErrorController  {
  
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
