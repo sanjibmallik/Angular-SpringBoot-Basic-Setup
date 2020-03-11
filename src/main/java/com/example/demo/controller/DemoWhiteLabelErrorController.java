c1701e14ac01d2ea05118ca79446e0035be44fba
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2020-03-12 00:02:47 +0530

    save

--- src/main/java/com/example/demo/controller/DemoWhiteLabelErrorController.java
+++ src/main/java/com/example/demo/controller/DemoWhiteLabelErrorController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class DemoWhileLabelErrorController implements ErrorController  {
+public class DemoWhiteLabelErrorController implements ErrorController  {
  
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
